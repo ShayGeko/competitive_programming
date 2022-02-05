94c414b68b3c2b8e77f73c3ce612bd562344313f
shaygeko <shramko.georgiy@gmail.com>
2022-02-05 15:37:35 -0800

    :memo: updated template

--- template.cpp
+++ template.cpp
@@ -4,12 +4,22 @@ using namespace std;
 
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
+typedef long long ll;
 
 const int maxn = 1e5;
 const int INF = 1e9;
 
+void solve(){
+
+}
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
 
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
     return 0;
 }
