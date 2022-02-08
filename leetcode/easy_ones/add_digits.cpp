be731b888fcfc9d445f39df7a6e02aa0363514c4
shaygeko <shramko.georgiy@gmail.com>
2022-02-08 00:01:59 -0800

    :zap: optimized from O(log^2 N) to O(1)

--- leetcode/easy_ones/add_digits.cpp
+++ leetcode/easy_ones/add_digits.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if(num < 10) return num;
-        
-        int cnt = 0;
-        int cur = 0;
-        int sum = 0;
-        while(num >= 10){
-            while(num){
-                cur+= num % 10;
-                num /= 10;
-            }
-            num = cur;
-            cur = 0;
-        }
-        
-        return num;
+      if(num==0)
+          return 0;
+      else if(num%9==0)
+          return 9;
+      else
+          return num%9;
     }
 };
