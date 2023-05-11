6c767294d00a5064a8eaae26ae41d78ffce83589
George Shramko <shramko.georgiy@gmail.com>
2023-05-11 00:00:57 -0700

    :zap: Summer 2023 sfu practice #1

---
+++ vjudge/2023/spring/17/d.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define has(a, b) (a.find(b) != a.end())
+#define imin(a, b) a = min(a, b)
+#define imax(a, b) a = max(a, b)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define fora(i, n) for(int i = 0; i < n; i++)
+#define forb(i, n) for(int i = 1; i <= n; i++)
+#define forc(a, b) for(const auto &a : b)
+#define ford(i, n) for(int i = n; i >= 0; i--)
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+template <class T>
+istream &operator >> (istream& is, pair<T, T> &p){
+    is >> p.first >> p.second;
+    return is;
+}
+template <class T>
+ostream &operator << (ostream& os, pair<T, T> &p){
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class T>
+pair<T, T> operator + (pair<T,T> &a, pair<T,T> &b){
+    return {a.first + b.first, a.second + b.second};
+}
+
+template <class T>
+pair<T, T> operator - (pair<T,T> &a, pair<T,T> &b){
+    return {a.first - b.first, a.second - b.second};
+}
+
+template<class T>
+vector<T> operator+(const vector<T> &a, const vector<T> &b){
+    assert(sz(a) == sz(b));
+
+    vector<T> res(a);
+    fora(i, sz(b)){
+        res[i] += b[i];
+    }
+
+    return res;
+}
+
+const int maxn = 1e5;
+const int INF = 2e9;
+const int p = 1e9+7;
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vi nums(n),order = {0, 1, 2}, lookup(3);
+    for(auto &x : nums) cin >> x;
+    
+    int best = 0;
+    do {
+        rep(i, 0, 3) lookup[order[i]] = i;
+        vi dp(3, 0);
+
+        rep(i, 0, sz(nums)) {
+            int x = lookup[nums[i]];
+            rep(j, x+1,3){
+                if(dp[j]) {
+                    dp[j]--;
+                    break;
+                }
+            }
+
+            ++dp[x];
+        }
+        best = max(best, accumulate(all(dp), 0));
+    } while(next_permutation(all(order)));
+
+    cout << best << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
+
+    // cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    cout.flush();
+    
+    return 0;
+}
