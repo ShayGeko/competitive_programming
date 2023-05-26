b2fdc238bff44f7aff8551617ab2e4b1fe4c164b
George Shramko <shramko.georgiy@gmail.com>
2023-05-26 14:33:26 -0700

    :sparkles: Intermediate contest 3

---
+++ vjudge/2023/summer/3-interm/c.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define has(a, b) (a.find(b) != a.end())
+#define imin(a, b) a = min(a, b)
+#define imax(a, b) a = max(a, b)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define fora(i, n) for(int i = 0; i < n; i++)
+#define forb(i, n) for(int i = 1; i <= n; i++)
+#define forc(a, b) for(const auto &a : b)
+#define ford(i, n) for(int i = n; i >= 0; i--)
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+template <class T>
+istream &operator >> (istream& is, pair<T, T> &p){
+    is >> p.first >> p.second;
+    return is;
+}
+template <class T>
+ostream &operator << (ostream& os, pair<T, T> &p){
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class T>
+pair<T, T> operator + (pair<T,T> &a, pair<T,T> &b){
+    return {a.first + b.first, a.second + b.second};
+}
+
+template <class T>
+pair<T, T> operator - (pair<T,T> &a, pair<T,T> &b){
+    return {a.first - b.first, a.second - b.second};
+}
+
+template<class T>
+vector<T> operator+(const vector<T> &a, const vector<T> &b){
+    assert(sz(a) == sz(b));
+
+    vector<T> res(a);
+    fora(i, sz(b)){
+        res[i] += b[i];
+    }
+
+    return res;
+}
+
+const int maxn = 1e5;
+const int INF = 2e9;
+const int p = 1e9+7;
+
+void solve(int n){
+    vi nums1(n), nums2(n);
+    for(auto &x : nums1) cin >> x;
+    for(auto &x : nums2) cin >> x;
+
+    vi sorted1 = nums1, sorted2 = nums2;
+    sort(all(sorted1));
+    sort(all(sorted2));
+
+    unordered_map<int, int> mp;
+    rep(i, 0, n) mp[sorted1[i]] = sorted2[i];
+
+    for(auto &x : nums1) cout << mp[x] << '\n';
+    cout << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
+
+    
+    while(cin>>t){
+        if(t==0) break;
+        solve(t);
+    }
+    cout.flush();
+    
+    return 0;
+}
---
+++ vjudge/2023/summer/3-interm/f.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define has(a, b) (a.find(b) != a.end())
+#define imin(a, b) a = min(a, b)
+#define imax(a, b) a = max(a, b)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define fora(i, n) for(int i = 0; i < n; i++)
+#define forb(i, n) for(int i = 1; i <= n; i++)
+#define forc(a, b) for(const auto &a : b)
+#define ford(i, n) for(int i = n; i >= 0; i--)
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+template <class T>
+istream &operator >> (istream& is, pair<T, T> &p){
+    is >> p.first >> p.second;
+    return is;
+}
+template <class T>
+ostream &operator << (ostream& os, pair<T, T> &p){
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class T>
+pair<T, T> operator + (pair<T,T> &a, pair<T,T> &b){
+    return {a.first + b.first, a.second + b.second};
+}
+
+template <class T>
+pair<T, T> operator - (pair<T,T> &a, pair<T,T> &b){
+    return {a.first - b.first, a.second - b.second};
+}
+
+template<class T>
+vector<T> operator+(const vector<T> &a, const vector<T> &b){
+    assert(sz(a) == sz(b));
+
+    vector<T> res(a);
+    fora(i, sz(b)){
+        res[i] += b[i];
+    }
+
+    return res;
+}
+
+const int maxn = 1e5;
+const int INF = 2e9;
+const int p = 1e9+7;
+
+void solve(){
+    int n;
+    cin >> n;
+    vvi adj(n, vi(n));
+    rep(i, 0, n) rep(j, 0, n) cin >> adj[i][j];
+    int cnt = 0;
+
+    rep(i, 0, n){
+        rep(j, i + 1, n){
+            if(!adj[i][j]) continue;
+            rep(k, j+1, n) {
+                if(!adj[i][k]) continue;
+                if(adj[j][k])++cnt;
+            }
+        }
+    }
+
+    cout << cnt << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
+
+    // cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    cout.flush();
+    
+    return 0;
+}
