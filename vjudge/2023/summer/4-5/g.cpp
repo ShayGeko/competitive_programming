405450263d8ca418f01a687f9b3ffcdd63c0e343
George Shramko <shramko.georgiy@gmail.com>
2023-06-12 01:59:07 -0700

    :sparkles: solved triangle containment

---
+++ vjudge/2023/summer/4-5/g.cpp
@@ -0,0 +1,174 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define has(a, b) (a.find(b) != a.end())
+#define imin(a, b) a = min(a, b)
+#define imax(a, b) a = max(a, b)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define fora(i, n) for(int i = 0; i < n; i++)
+#define forb(i, n) for(int i = 1; i <= n; i++)
+#define forc(a, b) for(const auto &a : b)
+#define ford(i, n) for(int i = n; i >= 0; i--)
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+template <class T>
+istream &operator >> (istream& is, pair<T, T> &p){
+    is >> p.first >> p.second;
+    return is;
+}
+template <class T>
+ostream &operator << (ostream& os, pair<T, T> &p){
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class T>
+pair<T, T> operator + (pair<T,T> &a, pair<T,T> &b){
+    return {a.first + b.first, a.second + b.second};
+}
+
+template <class T>
+pair<T, T> operator - (pair<T,T> &a, pair<T,T> &b){
+    return {a.first - b.first, a.second - b.second};
+}
+
+template<class T>
+vector<T> operator+(const vector<T> &a, const vector<T> &b){
+    assert(sz(a) == sz(b));
+
+    vector<T> res(a);
+    fora(i, sz(b)){
+        res[i] += b[i];
+    }
+
+    return res;
+}
+
+const int maxn = 1e5;
+const int INF = 2e9;
+const int p = 1e9+7;
+
+template <class T> int sgn(T x) { return (x > 0) - (x < 0); }
+template<class T>
+struct Point {
+	typedef Point P;
+	T x, y;
+	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
+	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
+	P operator+(P p) const { return P(x+p.x, y+p.y); }
+	P operator-(P p) const { return P(x-p.x, y-p.y); }
+	P operator*(T d) const { return P(x*d, y*d); }
+	P operator/(T d) const { return P(x/d, y/d); }
+	T dot(P p) const { return x*p.x + y*p.y; }
+	T cross(P p) const { return x*p.y - y*p.x; }
+	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
+	T dist2() const { return x*x + y*y; }
+	double dist() const { return sqrt((double)dist2()); }
+	// angle to x-axis in interval [-pi, pi]
+	long double angle() const { return atan2l(y, x); }
+	P unit() const { return *this/dist(); } // makes dist()=1
+	P perp() const { return P(-y, x); } // rotates +90 degrees
+	P normal() const { return perp().unit(); }
+	// returns point rotated 'a' radians ccw around the origin
+	P rotate(double a) const {
+		return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	friend ostream& operator<<(ostream& os, P p) {
+		return os << "(" << p.x << "," << p.y << ")"; }
+};
+
+struct Tree {
+	typedef ll T;
+	static constexpr T unit = 0;
+	T f(T a, T b) { return a+b; } // (any associative fn)
+	vector<T> s; int n;
+	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
+	void update(int pos, T val) {
+		for (s[pos += n] = val; pos /= 2;)
+			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
+	}
+	T query(int b, int e) { // query [b, e)
+		T ra = unit, rb = unit;
+		for (b += n, e += n; b < e; b /= 2, e /= 2) {
+			if (b % 2) ra = f(ra, s[b++]);
+			if (e % 2) rb = f(s[--e], rb);
+		}
+		return f(ra, rb);
+	}
+};
+
+Point<long double> a, b;
+void solve(){
+    int n, x1;
+    cin >> n >> x1;
+    a = Point<long double>(0, 0);
+    b = Point<long double> (x1, 0);
+    vector<pair<Point<long double>, pii>> pts(n);
+    rep(i, 0, sz(pts)){
+        int x, y, cost;
+        cin >> x >> y >> cost;
+        pts[i]={Point<long double>(x,y), {cost, i}};
+    }
+    
+    sort(all(pts), [](pair<Point<long double>, pii> &pta, pair<Point<long double>, pii> &ptb){
+        return pta.fi.angle() < ptb.fi.angle();
+    });
+    
+    vi anglePos(n);
+    rep(i, 0, sz(pts)){
+        anglePos[pts[i].se.se] = i;
+    }
+    auto ptsB = pts;
+    // sort points by angle descending from point (x, 0)
+    sort(all(ptsB), [](pair<Point<long double>, pii> &pta, pair<Point<long double>, pii> &ptb){
+        auto ptaFromB = pta.fi - b;
+        auto ptbFromB = ptb.fi -b;
+        return ptaFromB.angle() > ptbFromB.angle();
+    });
+    // Go through points in order sorted by angle
+    // Point A is only in triangle with Point B as vertex if A was already seen in this traversal, and theta(A) < theta(B) from origin
+
+    vll ans(n);
+    Tree segtree(n+1);
+    for(auto &x : ptsB){
+        // cout << x.fi.x << " " << x.fi.y << " " << (x.fi - b).angle() << '\n';
+        // angle is from 0 to pi (only I and II quadrants) 
+        ans[x.se.se] = segtree.query(0, anglePos[x.se.se]);
+        segtree.update(anglePos[x.se.se], x.se.fi);
+    }
+
+    for(auto &x : ans) cout << x << '\n';
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
+
+    // cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    cout.flush();
+    
+    return 0;
+}
