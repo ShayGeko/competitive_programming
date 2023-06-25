2a6c76e1e65f2803660a61751108881266a1d68f
George Shramko <shramko.georgiy@gmail.com>
2023-06-25 00:03:49 -0700

    Improving complexity

--- leetcode/graphs/count_all_possible_routes.cpp
+++ leetcode/graphs/count_all_possible_routes.cpp
@@ -5,47 +5,39 @@ using namespace std;
 class Solution {
 public:
     const int p = 1e9+7;
+    int n;
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-        int n = locations.size();
-        // [location][len][fuel]
-        vector<unordered_map<int, unordered_map<int,int>>> ways(n);
-        vector<vector<int>> d(n, vector<int>(n));
-        for(int i = 0; i < n; ++i) for(int j = i; j < n; ++j) {
-            int dist = locations[i] - locations[j];
-            if(dist < 0) dist = -dist;
-            d[i][j] = d[j][i] = dist;
-        }
+        n = locations.size();
+        vector<vector<int>> dp(n, vector<int>(fuel, -1));
 
-        ways[0][start][fuel] = 1;
+        return rec_count(dp, locations, start, fuel, finish);
+    }
+    int rec_count(vector<vector<int>> &dp, vector<int>& locations, int v, int fuel, int finish){
+        if(dp[v][fuel] >= 0) return dp[v][fuel];
+
+
         int ans = 0;
-        // [location, length, fuel] 
-        queue<vector<int>> q;
-        q.push({start, 0, fuel});
-        while(!q.empty()) {
-            auto v = q.front();q.pop();
-            int v_ways = ways[v[0]][v[1]][v[2]];
-            if(v[0] == finish) ans = (ans + v_ways) % p;
+        for(int i = 0; i < n; ++i){
+            if(i == v) continue;
 
-            for(int i = 0; i < n; ++i) {
-                if(i == v[0]) continue;
-
-                if(v[2]  - d[v[0]][i] >= 0) {
-                    vector<int> to = {i, v[1]+1, v[2] - d[v[0]][i]};
-                    int& to_ways = ways[to[0]][to[1]][to[2]];
-                    
-                    if(to_ways == 0) q.push(to);
-                    to_ways = (to_ways + v_ways) % p;
-                }
+            int cost = abs(locations[v]-locations[i]);
+            if(fuel - cost >= 0){
+                ans = (rec_count(dp, locations, i, fuel - cost, finish) + ans) % p;
             }
         }
 
-        return ans;
+        if(v == finish){
+            ans = (ans + 1) % p;
+        }
+        return dp[v][fuel] = ans;
     }
 };
 
 int main() {
     Solution s;
-    vector<int> locations = {2,3,6,8,4};
-    int start = 1, finish = 3, fuel = 5;
+    vector<int> locations(100);
+    int start = 1, finish = 2, fuel = 200;
+
+    for(int i = 1; i <= 100; ++i) locations[i-1]=i;
     cout << s.countRoutes(locations, start, finish, fuel) << endl;
 }
