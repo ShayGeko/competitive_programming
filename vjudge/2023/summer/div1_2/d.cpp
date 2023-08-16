bca4219e4c6c4f9fbb632b2b846c2187a950d22f
George Shramko <shramko.georgiy@gmail.com>
2023-08-16 16:13:03 -0700

    Curveknights

---
+++ vjudge/2023/summer/div1_2/d.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define has(a, b) (a.find(b) != a.end())
+#define imin(a, b) a = min(a, b)
+#define imax(a, b) a = max(a, b)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define fora(i, n) for(int i = 0; i < n; i++)
+#define forb(i, n) for(int i = 1; i <= n; i++)
+#define forc(a, b) for(const auto &a : b)
+#define ford(i, n) for(int i = n; i >= 0; i--)
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+template <class T>
+istream &operator >> (istream& is, pair<T, T> &p){
+    is >> p.first >> p.second;
+    return is;
+}
+template <class T>
+ostream &operator << (ostream& os, pair<T, T> &p){
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class T>
+pair<T, T> operator + (pair<T,T> &a, pair<T,T> &b){
+    return {a.first + b.first, a.second + b.second};
+}
+
+template <class T>
+pair<T, T> operator - (pair<T,T> &a, pair<T,T> &b){
+    return {a.first - b.first, a.second - b.second};
+}
+
+template<class T>
+vector<T> operator+(const vector<T> &a, const vector<T> &b){
+    assert(sz(a) == sz(b));
+
+    vector<T> res(a);
+    fora(i, sz(b)){
+        res[i] += b[i];
+    }
+
+    return res;
+}
+
+const int maxn = 1e5;
+const int INF = 2e9;
+const int p = 1e9+7;
+
+vi demand;
+vector<vpii> dependencies;
+vector<vll> cost;
+vll ans;
+int n;
+vll getCost(int v, int count) {
+    if(!cost[v].size()) {
+        cost[v]=vll(n);
+        for(auto dep: dependencies[v]) {
+            vll addCost = getCost(dep.first, dep.second);
+            rep(i, 0, n) cost[v][i] += addCost[i];
+        }
+        cost[v][v]=1;
+    }
+
+    vll resourceList = cost[v];
+    for(auto &x : resourceList) x*=count;
+    return resourceList;
+}
+void solve(){
+    int m;
+    cin >> n >> m;
+
+    demand = vi(n);
+    rep(i, 0, n) cin >> demand[i];
+
+    
+    dependencies = vector<vpii>(n);
+    rep(i, 0, m) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        dependencies[v].emplace_back(u, w);
+    }
+
+    cost = vector<vll>(n);
+    ans = vll(n);
+
+    rep(i, 0, n) {
+        if(demand[i]) {
+            vll addCost = getCost(i, demand[i]);
+            rep(i, 0, n) ans[i]+=addCost[i];
+        }
+    }
+
+    rep(i, 0, n) {
+        cout << ans[i] << " ";
+    }
+    cout << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
+
+    // cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    cout.flush();
+    
+    return 0;
+}
