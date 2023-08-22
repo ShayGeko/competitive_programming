38b02a788278c3317a2392884b6f591fb71ca131
George Shramko <shramko.georgiy@gmail.com>
2023-08-21 22:40:30 -0700

    CSES 1623 (Apple division)

---
+++ vjudge/2023/summer/div1_4/e.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define has(a, b) (a.find(b) != a.end())
+#define imin(a, b) a = min(a, b)
+#define imax(a, b) a = max(a, b)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define fora(i, n) for(int i = 0; i < n; i++)
+#define forb(i, n) for(int i = 1; i <= n; i++)
+#define forc(a, b) for(const auto &a : b)
+#define ford(i, n) for(int i = n; i >= 0; i--)
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+template <class T>
+istream &operator >> (istream& is, pair<T, T> &p){
+    is >> p.first >> p.second;
+    return is;
+}
+template <class T>
+ostream &operator << (ostream& os, pair<T, T> &p){
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class T>
+pair<T, T> operator + (pair<T,T> &a, pair<T,T> &b){
+    return {a.first + b.first, a.second + b.second};
+}
+
+template <class T>
+pair<T, T> operator - (pair<T,T> &a, pair<T,T> &b){
+    return {a.first - b.first, a.second - b.second};
+}
+
+template<class T>
+vector<T> operator+(const vector<T> &a, const vector<T> &b){
+    assert(sz(a) == sz(b));
+
+    vector<T> res(a);
+    fora(i, sz(b)){
+        res[i] += b[i];
+    }
+
+    return res;
+}
+
+const int maxn = 1e5;
+const int INF = 2e9;
+const int p = 1e9+7;
+
+void solve(){
+    int n;
+    cin >> n;
+    vi nums(n);
+    for(auto &x : nums) cin >> x;
+
+    int maskBound = 1 << n;
+    ll sum = 0;
+    for(auto x : nums) {
+        sum += x;
+    }
+    ll ans = 1e10;
+    rep(mask, 0, maskBound) { 
+        ll curSum = 0;
+        rep(i, 0, n) {
+            if(mask & (1<<i)) {
+                curSum += nums[i];
+            }
+        }
+
+        ans = min(ans, abs(sum - 2*curSum));
+    }
+
+    cout << ans << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
+
+    // cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    cout.flush();
+    
+    return 0;
+}
