978b59691382d507bb7bb19a6d25ed1ef0319f8f
George Shramko <shramko.georgiy@gmail.com>
2023-08-29 00:26:24 -0700

    UVA Factors and multiples

---
+++ vjudge/2023/summer/div1_flow_training/UVA_11159_Factors_And_Multiples.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define has(a, b) (a.find(b) != a.end())
+#define imin(a, b) a = min(a, b)
+#define imax(a, b) a = max(a, b)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define fora(i, n) for(int i = 0; i < n; i++)
+#define forb(i, n) for(int i = 1; i <= n; i++)
+#define forc(a, b) for(const auto &a : b)
+#define ford(i, n) for(int i = n; i >= 0; i--)
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+template <class T>
+istream &operator >> (istream& is, pair<T, T> &p){
+    is >> p.first >> p.second;
+    return is;
+}
+template <class T>
+ostream &operator << (ostream& os, pair<T, T> &p){
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class T>
+pair<T, T> operator + (pair<T,T> &a, pair<T,T> &b){
+    return {a.first + b.first, a.second + b.second};
+}
+
+template <class T>
+pair<T, T> operator - (pair<T,T> &a, pair<T,T> &b){
+    return {a.first - b.first, a.second - b.second};
+}
+
+template<class T>
+vector<T> operator+(const vector<T> &a, const vector<T> &b){
+    assert(sz(a) == sz(b));
+
+    vector<T> res(a);
+    fora(i, sz(b)){
+        res[i] += b[i];
+    }
+
+    return res;
+}
+
+const int maxn = 1e5;
+const int INF = 2e9;
+const int p = 1e9+7;
+
+struct Dinic {
+    struct Edge {
+        int to, rev;
+        ll c, oc;
+        ll flow() { return max(oc - c, 0LL); } // if you need flows
+    };
+    vi lvl, ptr, q;
+    vector<vector<Edge>> adj;
+    Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
+    void addEdge(int a, int b, ll c, ll rcap = 0) {
+        adj[a].push_back({b, sz(adj[b]), c, c});
+        adj[b].push_back({a, sz(adj[a]) - 1, rcap, rcap});
+    }
+    ll dfs(int v, int t, ll f) {
+        if (v == t || !f) return f;
+        for (int& i = ptr[v]; i < sz(adj[v]); i++) {
+            Edge& e = adj[v][i];
+            if (lvl[e.to] == lvl[v] + 1)
+                if (ll p = dfs(e.to, t, min(f, e.c))) {
+                    e.c -= p, adj[e.to][e.rev].c += p;
+                    return p;
+                }
+        }
+        return 0;
+    }
+    ll calc(int s, int t) {
+        ll flow = 0; q[0] = s;
+        rep(L,0,31) do { // 'int L=30' maybe faster for random data
+                lvl = ptr = vi(sz(q));
+                int qi = 0, qe = lvl[s] = 1;
+                while (qi < qe && !lvl[t]) {
+                    int v = q[qi++];
+                    for (Edge e : adj[v])
+                        if (!lvl[e.to] && e.c >> (30 - L))
+                            q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
+                }
+                while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
+            } while (lvl[t]);
+        return flow;
+    }
+    bool leftOfMinCut(int a) { return lvl[a] != 0; }
+};
+
+void solve(int casenum){
+    int n, m;
+    cin >> n;
+    vi A(n);
+    rep(i, 0, n) cin >> A[i];
+    cin >> m;
+    vi B(m);
+    rep(i,0,m)cin >> B[i];
+    int s = n + m, t = n +m + 1;
+
+    Dinic dinic(n+m+2);
+    rep(i, 0, n) {
+        // max bipartite matching with (A_i, B_j) in E if B[j]%A[i]==0
+        dinic.addEdge(s, i, 1);
+        rep(j, 0, m) {
+            if(B[j] == 0 || (A[i] && B[j] % A[i] == 0)){
+                dinic.addEdge(i, n+j, 1);
+            }
+        }
+    }
+    rep(j, 0, m) dinic.addEdge(n+j, t, 1);
+
+    cout << "Case " << casenum << ": " << dinic.calc(s, t) << endl;
+}
+
+int main(){
+//    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t = 1;
+
+     cin >> t;
+    
+    rep(i, 0, t){
+        solve(i+1);
+    }
+    cout.flush();
+    
+    return 0;
+}
